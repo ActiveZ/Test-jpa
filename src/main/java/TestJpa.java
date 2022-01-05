4584448597f97634fbf80eab32a905697008cc98
ActiveZ <activez.git@gmail.com>
2022-01-05 17:43:46 +0100

    tp2 ok

--- src/main/java/TestJpa.java
+++ src/main/java/TestJpa.java
@@ -1,5 +1,4 @@
 import entities.Livre;
-import entities.Pizza;
 
 import javax.persistence.*;
 import java.util.List;
@@ -51,13 +50,37 @@ public class TestJpa {
             em.persist(newLivre);
             System.out.println("insertion d'un nouveau livre avec l'id = " + newLivre.getId());
 
-          // Modifiez le titre du livre d’identifiant 5 qui possède une erreur : le nouveau titre doit être
+            // Modifiez le titre du livre d’identifiant 5 qui possède une erreur : le nouveau titre doit être
             // « Du plaisir dans la cuisine » au lieu de « 1001 recettes de Cuisine ».
             Livre l5 = em.find(Livre.class, 5);
             l5.setTitre("Du plaisir dans la cuisine");
 
+
+//           Faites une requête JPQL pour extraire de la base un livre en fonction de son titre.
+            Query query = em.createQuery("SELECT l FROM Livre l WHERE l.titre='Germinal'");
+            List<Livre> result1 = query.getResultList();
+            System.out.println("lQuery.get(0) Titre: " + result1.get(0).getTitre() + " -- " + result1.get(0).getAuteur());
+
+//           Faites une requête JPQL pour extraire de la base un livre en fonction de son auteur.
+            query = em.createQuery("SELECT l FROM Livre l WHERE l.auteur='Jules Verne'");
+            List<Livre> result2 = query.getResultList();
+            System.out.println("lQuery.get(0) Titre: " + result2.get(0).getTitre() + " -- " + result2.get(0).getAuteur());
+
+//           Supprimez un livre de votre choix en base de données.
+            Livre lDead = em.find(Livre.class, 4);
+            em.remove(lDead);
+
+//           Affichez la liste de tous les livres présents en base de données (titre et auteur).
+            System.out.println("///////// liste des livres ///////////");
+            TypedQuery<Livre> queryAll = em.createQuery("select l from Livre l", Livre.class);
+            List<Livre> listeLivres = queryAll.getResultList();
+            for (Livre l : listeLivres) {
+                System.out.println(l.getTitre() + " -- " + l.getAuteur());
+            }
+
             tx.commit();
             em.close();
+
         } catch (PersistenceException e) {
             System.err.println("Erreur de persistance : " + e.getMessage());
         } finally {
