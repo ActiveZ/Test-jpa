dca8e8baa57b88df8e95980d78ee4dee2edce43c
ActiveZ <activez.git@gmail.com>
2022-01-05 16:54:06 +0100

    tp2 insetion et modification

--- src/main/java/TestJpa.java
+++ src/main/java/TestJpa.java
@@ -1,3 +1,4 @@
+import entities.Livre;
 import entities.Pizza;
 
 import javax.persistence.*;
@@ -8,31 +9,58 @@ public class TestJpa {
 
         EntityManagerFactory emf = null;
 
+        //////////// TP 01 //////////////
+//        try {
+//            emf = Persistence.createEntityManagerFactory("pizza-pu");
+//            EntityManager em = emf.createEntityManager();
+//            Pizza pizzaId1 = em.find(Pizza.class, 1);
+//            System.out.println("Pizza id=1 name=" + pizzaId1.getName());
+//            TypedQuery<Pizza> query = em.createQuery("select p from Pizza p", Pizza.class);
+//            List<Pizza> listePizzas = query.getResultList();
+//            for (Pizza pizza : listePizzas) {
+//                System.out.println(pizza.getName());
+//            }
+//            em.close();
+//            System.out.println("Pizza id=1 name=" + pizzaId1.getName());
+//        }
+//        catch (PersistenceException e) {
+//            System.err.println("Erreur de persistance : " + e.getMessage());
+//        }
+//        finally {
+//            if (emf != null) {
+//                emf.close();
+//            }
+//        }
+
+
+        ///////// TP 02 /////////
         try {
             emf = Persistence.createEntityManagerFactory("pizza-pu");
-
             EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
 
-            Pizza pizzaId1 = em.find(Pizza.class, 1);
-            System.out.println("Pizza id=1 name=" + pizzaId1.getName());
+            // récupération du livre avec id = 3
+            Livre livre3 = em.find(Livre.class, 3);
+            System.out.println("Livre id = 3: " + livre3.getTitre() + " -- " + livre3.getAuteur());
 
-            TypedQuery<Pizza> query = em.createQuery("select p from Pizza p", Pizza.class);
+            // insertion d'un nouveau livre
+            Livre newLivre = new Livre();
+            newLivre.setTitre("Tintin au Congo");
+            newLivre.setAuteur("Hergé");
+            em.persist(newLivre);
+            System.out.println("insertion d'un nouveau livre avec l'id = " + newLivre.getId());
 
-            List<Pizza> listePizzas = query.getResultList();
+          // Modifiez le titre du livre d’identifiant 5 qui possède une erreur : le nouveau titre doit être
+            // « Du plaisir dans la cuisine » au lieu de « 1001 recettes de Cuisine ».
+            Livre l5 = em.find(Livre.class, 5);
+            l5.setTitre("Du plaisir dans la cuisine");
 
-            for (Pizza pizza : listePizzas) {
-                System.out.println(pizza.getName());
-            }
-
+            tx.commit();
             em.close();
-
-            System.out.println("Pizza id=1 name=" + pizzaId1.getName());
-        }
-        catch (PersistenceException e) {
+        } catch (PersistenceException e) {
             System.err.println("Erreur de persistance : " + e.getMessage());
-        }
-
-        finally {
+        } finally {
             if (emf != null) {
                 emf.close();
             }
---
+++ src/main/java/entities/Livre.java
@@ -0,0 +1,41 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "LIVRE")
+public class Livre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "TITRE", length = 255, nullable = false)
+    private String titre;
+
+    @Column(name = "AUTEUR", length = 50, nullable = false)
+    private String auteur;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+}
